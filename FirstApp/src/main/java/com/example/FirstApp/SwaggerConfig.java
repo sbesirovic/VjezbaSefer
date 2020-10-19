0c0bb4738ea98e6e27991ba5bdcd3062a566b273
Edi Pencl <edivisoko@hotmail.com>
2020-10-19 11:20:16 +0200

    test

--- FirstApp/src/main/java/com/example/FirstApp/SwaggerConfig.java
+++ FirstApp/src/main/java/com/example/FirstApp/SwaggerConfig.java
@@ -1,7 +1,6 @@
 package com.example.FirstApp;
-//testiram
+
 import com.google.common.collect.Lists;
-import io.swagger.annotations.ApiOperation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -17,7 +16,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import java.util.List;
 
 
-
+@Configuration
 @EnableSwagger2
 public class SwaggerConfig {
 
